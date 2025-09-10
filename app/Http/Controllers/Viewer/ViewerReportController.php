3b8c40539cd81d8d7022288dcfb4a8baed7981f0
salmanursholihah <salmanursholihah@gmail.com>
2025-09-10 08:48:47 +0700

    audit controller

--- app/Http/Controllers/Viewer/ViewerReportController.php
+++ app/Http/Controllers/Viewer/ViewerReportController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers\Viewer;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Report;
 
 class ViewerReportController extends Controller
 {
-      public function index() {
-        $reports = Report::paginate(10);
+    public function index()
+    {
+        $reports = Report::latest()->paginate(5);
         return view('backend.viewer.reports.index', compact('reports'));
+    }
+
+    public function show(Report $report)
+    {
+        return view('backend.viewer.reports.show', compact('report'));
     }
 }
